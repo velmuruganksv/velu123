c2edfdb723489aa7d817e75ff600be18e5a0e2dc
velmuruganksv <velmurugan.nellai9360@gmail.com>
2025-09-19 09:47:30 +0530

    Add files via upload

---
+++ velu.js
@@ -0,0 +1,20 @@
+a=100
+b=200
+console.log("Addition :",a+b)
+console.log("Subtraction:",a-b)
+console.log("Multiplication:",a*b)
+console.log("Division:",a/b)
+c=a++
+d=a--
+d1=a--
+e=++a
+f=--a
+console.log("INCREMENT/DECREMENT OPERATIONS")
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(d1)    
+console.log(f)
+console.log(b)
+console.log(a)
